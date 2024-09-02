6e5f83a5373176a37347c8fe14853914afb5a1ee
Noat Eden <noat.eth@gmail.com>
2024-09-03 04:17:39 +0700

    collect last 24h data

--- src/cmd/run.ts
+++ src/cmd/run.ts
@@ -1,4 +1,5 @@
 import { ProtocolConfigs } from '../configs';
+import logger from '../lib/logger';
 import { sleep } from '../lib/utils';
 import { getProtocolAdapters } from '../modules/adapters';
 import { ContextServices, ContextStorages } from '../types/namespaces';
@@ -19,11 +20,20 @@ export class RunCommand extends BasicCommand {
     const storages: ContextStorages = await super.getStorages();
     const adapters = getProtocolAdapters(services, storages);
 
+    logger.info('attemp run command', {
+      service: 'command',
+      argvProtocol: argv.protocol,
+      argvService: argv.service,
+      argvFromTime: argv.fromTime,
+      argvForce: argv.force,
+    });
+
     do {
       for (const protocol of Object.keys(ProtocolConfigs as any)) {
         if (argv.protocol === undefined || argv.protocol === '' || argv.protocol === protocol) {
           if (adapters[protocol]) {
             await adapters[protocol].run({
+              service: argv.service === 'state' || argv.service === 'snapshot' ? argv.service : undefined,
               fromTime: argv.fromTime ? argv.fromTime : undefined,
               force: argv.force ? argv.force : false,
             });
@@ -46,6 +56,11 @@ export class RunCommand extends BasicCommand {
         default: '',
         describe:
           'Collect data of given protocol. You can pass a list of protocol seperated by comma, ex: --protocol "aavev3,uniswapv2".',
+      },
+      service: {
+        type: 'string',
+        default: 'undefined',
+        describe: 'Collect current data or historical snapshots or both.',
       },
       fromTime: {
         type: 'number',
--- src/modules/adapters/protocol.ts
+++ src/modules/adapters/protocol.ts
@@ -28,119 +28,144 @@ export default class ProtocolAdapter implements IProtocolAdapter {
   }
 
   public async run(options: RunAdapterOptions): Promise<void> {
-    this.executeSession.startSession('start to update protocol current data', {
-      service: this.name,
-      protocol: this.protocolConfig.protocol,
-      category: this.protocolConfig.category,
-    });
-
     //
     // collect current state data
     //
-    const currentTimestamp = getTimestamp();
-    const last24HoursTimestamp = currentTimestamp - TimeUnits.SecondsPerDay;
+    if (!options.service || options.service === 'state') {
+      this.executeSession.startSession('start to update protocol current data', {
+        service: this.name,
+        protocol: this.protocolConfig.protocol,
+        category: this.protocolConfig.category,
+      });
 
-    const last24HoursData = await this.getProtocolData({
-      timestamp: currentTimestamp,
-      beginTime: last24HoursTimestamp,
-      endTime: currentTimestamp,
-    });
-    if (last24HoursData) {
-      await this.storages.database.update({
-        collection: envConfig.mongodb.collections.protocolDataStates.name,
-        keys: {
-          protocol: last24HoursData.protocol,
-        },
-        updates: {
-          ...last24HoursData,
-        },
-        upsert: true,
+      const currentTimestamp = getTimestamp();
+      const last24HoursTimestamp = currentTimestamp - TimeUnits.SecondsPerDay;
+      const last48HoursTimestamp = last24HoursTimestamp - TimeUnits.SecondsPerDay;
+
+      const last24HoursData = await this.getProtocolData({
+        timestamp: currentTimestamp,
+        beginTime: last24HoursTimestamp,
+        endTime: currentTimestamp,
+      });
+      const last48HoursData = await this.getProtocolData({
+        timestamp: last24HoursTimestamp,
+        beginTime: last48HoursTimestamp,
+        endTime: last24HoursTimestamp,
+      });
+      if (last24HoursData) {
+        if (last48HoursData) {
+          await this.storages.database.update({
+            collection: envConfig.mongodb.collections.protocolDataStates.name,
+            keys: {
+              protocol: last24HoursData.protocol,
+            },
+            updates: {
+              ...last24HoursData,
+              last24HoursData: last48HoursData,
+            },
+            upsert: true,
+          });
+        } else {
+          await this.storages.database.update({
+            collection: envConfig.mongodb.collections.protocolDataStates.name,
+            keys: {
+              protocol: last24HoursData.protocol,
+            },
+            updates: {
+              ...last24HoursData,
+            },
+            upsert: true,
+          });
+        }
+      }
+
+      this.executeSession.endSession('updated current data for protocol', {
+        service: this.name,
+        protocol: this.protocolConfig.protocol,
+        category: this.protocolConfig.category,
       });
     }
-
-    this.executeSession.endSession('updated current data for protocol', {
-      service: this.name,
-      protocol: this.protocolConfig.protocol,
-      category: this.protocolConfig.category,
-    });
 
     //
     // collect snapshots
     //
-    let startTime = options.fromTime ? options.fromTime : this.protocolConfig.birthday;
+    if (!options.service || options.service === 'snapshot') {
+      const currentTimestamp = getTimestamp();
+      let startTime = options.fromTime ? options.fromTime : this.protocolConfig.birthday;
 
-    // we make sure startTime is start day (00:00 UTC+0) timestamp
-    startTime = getStartDayTimestamp(startTime);
+      // we make sure startTime is start day (00:00 UTC+0) timestamp
+      startTime = getStartDayTimestamp(startTime);
 
-    if (!options.force) {
-      // we find the latest snapshot timestamp
-      const latestSnapshot = (
-        await this.storages.database.query({
-          collection: envConfig.mongodb.collections.protocolDataSnapshots.name,
-          query: {
-            protocol: this.protocolConfig.protocol,
-          },
-          options: {
-            limit: 1,
-            skip: 0,
-            order: { timestamp: -1 },
-          },
-        })
-      )[0];
-      if (latestSnapshot) {
-        startTime = latestSnapshot.timestamp + TimeUnits.SecondsPerDay;
+      if (!options.force) {
+        // we find the latest snapshot timestamp
+        const latestSnapshot = (
+          await this.storages.database.query({
+            collection: envConfig.mongodb.collections.protocolDataSnapshots.name,
+            query: {
+              protocol: this.protocolConfig.protocol,
+            },
+            options: {
+              limit: 1,
+              skip: 0,
+              order: { timestamp: -1 },
+            },
+          })
+        )[0];
+        if (latestSnapshot) {
+          startTime = latestSnapshot.timestamp + TimeUnits.SecondsPerDay;
+        }
       }
-    }
 
-    const todayTimestamp = getTodayUTCTimestamp();
-    logger.info('start to update protocol snapshots data', {
-      service: this.name,
-      protocol: this.protocolConfig.protocol,
-      category: this.protocolConfig.category,
-      fromDate: getDateString(startTime),
-      toDate: getDateString(todayTimestamp),
-    });
-
-    while (startTime <= todayTimestamp) {
-      this.executeSession.startSession('start to update protocol snapshot', {
+      const todayTimestamp = getTodayUTCTimestamp();
+      logger.info('start to update protocol snapshots data', {
         service: this.name,
         protocol: this.protocolConfig.protocol,
         category: this.protocolConfig.category,
-        date: getDateString(startTime),
+        fromDate: getDateString(startTime),
+        toDate: getDateString(todayTimestamp),
       });
 
-      let endTime = startTime + TimeUnits.SecondsPerDay - 1;
-      if (endTime > currentTimestamp) {
-        endTime = currentTimestamp;
-      }
-
-      const dataTimeframe24Hours = await this.getProtocolData({
-        timestamp: startTime,
-        beginTime: startTime,
-        endTime: endTime,
-      });
-      if (dataTimeframe24Hours) {
-        await this.storages.database.update({
-          collection: envConfig.mongodb.collections.protocolDataSnapshots.name,
-          keys: {
-            protocol: dataTimeframe24Hours.protocol,
-            timestamp: dataTimeframe24Hours.timestamp,
-          },
-          updates: {
-            ...dataTimeframe24Hours,
-          },
-          upsert: true,
+      while (startTime <= todayTimestamp) {
+        this.executeSession.startSession('start to update protocol snapshot', {
+          service: this.name,
+          protocol: this.protocolConfig.protocol,
+          category: this.protocolConfig.category,
+          date: getDateString(startTime),
         });
+
+        let endTime = startTime + TimeUnits.SecondsPerDay - 1;
+        if (endTime > currentTimestamp) {
+          endTime = currentTimestamp;
+        }
+
+        const dataTimeframe24Hours = await this.getProtocolData({
+          timestamp: startTime,
+          beginTime: startTime,
+          endTime: endTime,
+        });
+        if (dataTimeframe24Hours) {
+          await this.storages.database.update({
+            collection: envConfig.mongodb.collections.protocolDataSnapshots.name,
+            keys: {
+              protocol: dataTimeframe24Hours.protocol,
+              timestamp: dataTimeframe24Hours.timestamp,
+            },
+            updates: {
+              ...dataTimeframe24Hours,
+            },
+            upsert: true,
+          });
+        }
+
+        this.executeSession.endSession('updated snapshot data for protocol', {
+          service: this.name,
+          protocol: this.protocolConfig.protocol,
+          category: this.protocolConfig.category,
+          date: getDateString(startTime),
+        });
+
+        startTime += TimeUnits.SecondsPerDay;
       }
-
-      this.executeSession.endSession('updated snapshot data for protocol', {
-        service: this.name,
-        protocol: this.protocolConfig.protocol,
-        category: this.protocolConfig.category,
-        date: getDateString(startTime),
-      });
-
-      startTime += TimeUnits.SecondsPerDay;
     }
   }
 
